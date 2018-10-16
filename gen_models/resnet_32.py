49cdf115234099a770ac5e507f1aa36a2c72cf0f
Mert Bülent Sarıyıldız <mbsariyildiz@gmail.com>
2018-10-16 14:01:22 +0300

    Add CIFAR10 support.

---
+++ gen_models/resnet_32.py
@@ -0,0 +1,49 @@
+import chainer
+import chainer.links as L
+from chainer import functions as F
+from gen_models.resblocks import Block
+from source.miscs.random_samples import sample_categorical, sample_continuous
+
+
+class ResNetGenerator32(chainer.Chain):
+    # Please set n_classes to 10 if train conditional GANs.
+    def __init__(self, ch=256, dim_z=128, bottom_width=4, activation=F.relu, n_classes=10,
+                 distribution="normal"):
+        super(ResNetGenerator32, self).__init__()
+        print(ch)
+        self.bottom_width = bottom_width
+        self.activation = activation
+        self.distribution = distribution
+        self.dim_z = dim_z
+        self.n_classes = n_classes
+        with self.init_scope():
+            self.l1 = L.Linear(dim_z, (bottom_width ** 2) * ch, initialW=chainer.initializers.GlorotUniform())
+            self.block2 = Block(ch, ch, activation=activation, upsample=True, n_classes=n_classes)
+            self.block3 = Block(ch, ch, activation=activation, upsample=True, n_classes=n_classes)
+            self.block4 = Block(ch, ch, activation=activation, upsample=True, n_classes=n_classes)
+            self.b5 = L.BatchNormalization(ch)
+            self.c5 = L.Convolution2D(ch, 3, ksize=3, stride=1, pad=1, initialW=chainer.initializers.GlorotUniform())
+
+    def sample_z(self, batchsize=64):
+        return sample_continuous(self.dim_z, batchsize, distribution=self.distribution, xp=self.xp)
+
+    def sample_y(self, batchsize=64):
+        return sample_categorical(self.n_classes, batchsize, distribution="uniform", xp=self.xp)
+
+    def __call__(self, batchsize=64, z=None, y=None):
+        if z is None:
+            z = self.sample_z(batchsize)
+        if y is None:
+            y = self.sample_y(batchsize) if self.n_classes > 0 else None
+        if (y is not None) and z.shape[0] != y.shape[0]:
+            raise ValueError('z.shape[0] != y.shape[0]')
+        h = z
+        h = self.l1(h)
+        h = F.reshape(h, (h.shape[0], -1, self.bottom_width, self.bottom_width))
+        h = self.block2(h, y)
+        h = self.block3(h, y)
+        h = self.block4(h, y)
+        h = self.b5(h)
+        h = self.activation(h)
+        h = F.tanh(self.c5(h))
+        return h
